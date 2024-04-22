7b1c140906ee5a0b6ba3dfabc152b0769554695c
Harshit Halwan <harshithalwan@gmail.com>
2024-04-22 19:47:15 +0530

    "camera position update"

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 camera.position.z = 9;
 camera.position.x = 3;
-camera.position.y = -2;
+camera.position.y = -1;
 camera.rotateZ(0.03)
 camera.rotateX(0.03)
 camera.rotateY(0.3)
